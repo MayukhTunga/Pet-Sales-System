38b63ab4c331a368ff11bc6c8f4032ac8f978ff1
Chitin1627 <pratyushprashob@gmail.com>
2023-04-28 08:42:59 +0530

    kuch to hai

--- backend/app.py
+++ backend/app.py
@@ -1,45 +1,22 @@
 from flask import Flask,request,render_template
-import flask
+from flask_mysqldb import MySQL
 import json
 from flask_cors import CORS
 import mysql.connector as sqltor
-# app = Flask(__name__)
-# CORS(app)
-
-# @app.route("/")
-# def hello():
-#     return "Hello, World!"
-
-# @app.route('/users', methods=["GET"])
-# def users():
-#     print("users endpoint reached...")
-#     with open("users.json", "r") as f:
-#         data = json.load(f)
-#         data.append({
-#             "username": "user4",
-#             "pets": ["hamster"]
-#         })
-
-#         return flask.jsonify(data)
-
+import pandas as pd
 
 app = Flask(__name__)
-@app.route('/')
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'root'
+app.config['MYSQL_DB'] = 'SMP_PET_SHOP'
+
+mysql = MySQL(app)
+
+
+@app.route('/', methods = ['GET','POST'])
 def index():
     return render_template('index.html')
-@app.route('/index2', methods = ['GET','POST'])
-def index2():
-    if(request=='POST'):
-        mycon = sqltor.connect(
-        host = 'localhost',
-        user = 'root',
-        password = 'root')
-        mycursor = mycon.cursor()
-        mycursor.execute("USE SMP_Pet_Shop;")
-        mycursor.execute(f"SELECT * FROM DOGS WHERE breed = '{Labrador}';")
-        data = mycursor.fetchall()
-        return data
-    return render_template('index2.html')
 
 
 if __name__ == "__main__":
