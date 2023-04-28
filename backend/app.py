a954a8aec81dac8746444a1ef2bf7f32516d153a
s1834 <s18324@protonmail.com>
2023-04-28 09:04:35 +0530

    :sparkles: Connected Flask and HTML

--- backend/app.py
+++ backend/app.py
@@ -13,14 +13,29 @@ app.config['MYSQL_DB'] = 'SMP_PET_SHOP'
 
 mysql = MySQL(app)
 
-
-@app.route('/')
+@app.route('/Labrador')
 def index():
-    return render_template('index.html')
+    return render_template('/../frontend/Labrador.html')
 
-@app.route('../frontend/Labrador.html')
+@app.route('/Husky')
 def index():
-    return render_template('Labrador.html')
+    return render_template('/../frontend/Husky.html')
+
+@app.route('/German Shepherd')
+def index():
+    return render_template('/../frontend/German Shepherd.html')
+
+@app.route('/Siamese Cat')
+def index():
+    return render_template('/../frontend/Siamese Cat.html')
+
+@app.route('/Persian Cat')
+def index():
+    return render_template('/../frontend/Persian Cat.html')
+
+@app.route('/British Short Hair')
+def index():
+    return render_template('/../frontend/British Short Hair.html')
 
 if __name__ == "__main__":
     app.run("localhost", 6969)
